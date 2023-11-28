c0a25c1f28df1aea7b915121448824b45596b6b3
gabriele-kaskeviciute <gabi.kaskeviciute@gmail.com>
2023-11-28 16:05:20 +0200

    JS9_DOM

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// 1 užduotis
+// H1 elemente parašykite savo vardą. Paspaudus ant vardo - tegul jūsų vardas atsiranda per vidurį ekrano, dvigubai didesniu font'o dydžiu ir raudona spalva.
+
+
+const myName = document.querySelector(".myName")
+
+
+
+myName.addEventListener("click", () => { 
+
+myName.style.color = "red";
+myName.style.fontSize = "4em";
+myName.style.textAlign = "center"
+
+
+
+});
+
